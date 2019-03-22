2082654b360dc11e0d32ddbdb02fef62ec6ee62e
Lucas Pedroso <lucas.pedroso@dextra-sw.com>
2019-03-21 23:45:05 -0300

    adding styled components for app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
+
+import styled, { keyframes } from "styled-components"
+
 import logo from './logo.svg';
-import './App.css';
+
+const Container = styled.div`
+	text-align: center
+`
+
+const Header = styled.header`
+	background-color: #222
+	height: 150px
+	padding: 20px
+	color: white
+`
+
+const rotate = keyframes`
+	from {
+		transform: rotate(0deg)
+	}
+
+	to {
+		transform: rotate(360deg)
+	}
+`
+
+const Image = styled.img`
+	animation: ${rotate} infinite 20s linear;
+	height: 80px;
+`
+
+const Title = styled.h1`
+	font-size: 1.5em;
+`
+
+const Description = styled.p`
+	font-size: large;
+`
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
+			<Container>
+				<Header>
+					<Image src={logo} alt="logo" />
+					<Title>Welcome to React</Title>
+				</Header>
+				<Description>
 					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-				<p> Heroku Test </p>
-			</div>
-		);
+				</Description>
+			</Container>
+		)
 	}
 }
 
