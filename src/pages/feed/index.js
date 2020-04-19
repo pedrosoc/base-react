2729a69749eedf3c0fe7df8406b7d7188bd1251f
Lucas Pedroso <lucas.pedroso@dextra-sw.com>
2020-04-19 17:51:57 -0300

    feat: project struct

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -2,11 +2,11 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
-import FeedNews from "../../features/news/components/Feed";
-import FeedSnippets from "../../features/snippets/components/Feed";
+import FeedNews from "@/features/news/components/feed";
+import FeedSnippets from "@/features/snippets/components/feed";
 
-import { postTypes } from "../../constants/postTypes";
-import { getLocationParam } from "../../utils/location";
+import { postTypes } from "@/constants/postTypes";
+import { getLocationParam } from "@/utils/location";
 
 export const Feed = ({ location }) => {
 	const type = getLocationParam(location, "type");
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Header from "../components/Header";
+import Header from "@/components/header";
 
 /* Pages */
 import About from "../pages/about";
