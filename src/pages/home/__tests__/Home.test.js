448547429a252c9039b7294bb760d0f30773eaa5
Lucas Pedroso <lucas.pedroso@dextra-sw.com>
2020-04-19 19:40:09 -0300

    fix: fixing dependecies and removing vulnerabilities

--- src/pages/home/__tests__/Home.test.js
+++ src/pages/home/__tests__/Home.test.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+
 import { Home } from "../";
 
-it("renders without crashing", () => {
-	const div = document.createElement("div");
-	ReactDOM.render(<Home />, div);
-	ReactDOM.unmountComponentAtNode(div);
+it("snapshot test for home component", () => {
+	const home = shallow(<Home />);
+	expect(home.getElement()).toMatchSnapshot();
 });
